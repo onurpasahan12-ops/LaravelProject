38fac3388fea6ffca0c034164f59522037c3e7b0
onurpasahan12-ops <onurpasahan12@gmail.com>
2026-06-11 13:07:23 +0300

    Fixed MassAssignmentException by adding fillable properties to Product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    use HasFactory;
+
+    // Veritabanına toplu olarak kaydedilmesine izin verdiğimiz sütunlar:
+    protected $fillable = [
+        'name',
+        'price',
+        'stock',
+        'description'
+    ];
 }
